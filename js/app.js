831da5f438606b6ed354f54deb748ce04fbbfbee
Ayoub Kalil <aubkalil@gmail.com>
2017-09-16 20:02:44 +0100

    cat clicker premium done but unorgnasied

--- js/app.js
+++ js/app.js
@@ -1,34 +1,73 @@
 (function(){
 	var cats = [{
-			name: "Kity",
-			alt: "Kity",
+			name: "Kitty",
+			alt: "Kitty",
 			src: "img/cat1.jpg",
 			counter : 0
 	},{
-			name: "Meosy",
-			alt: "Meosy",
+			name: "Tiger",
+			alt: "Tiger",
 			src: "img/cat2.jpg",
 			counter : 0
-	}];
+	},{
+			name: "Milo",
+			alt: "Milo",
+			src: "img/cat3.jpg",
+			counter : 0
+	},{
+			name: "Daisy",
+			alt: "Daisy",
+			src: "img/cat4.jpg",
+			counter : 0
+	},{
+			name: "Luna",
+			alt: "Luna",
+			src: "img/cat5.jpg",
+			counter : 0
+	}
+	];
+
+	
+
+	var catList = document.getElementById("catList"),
+		counterSelector = document.querySelector('#cat .counter'),
+		name = document.querySelector('#cat .catName'),
+		currentCat = cats[0],
+		img = document.querySelector('#cat .catImg');
+
 
 	for(i = 0 ; i<cats.length ; i++){
-		var cat = '#cat' + i,
-			counterSelector = document.querySelector(cat + ' .counter'),
-			name = document.querySelector(cat + ' .catName'),
-			img = document.querySelector(cat + ' .catImg');
+		
+		var	cat = cats[i],
+			
 
-		name.innerText = cats[i].name;
-		img.src = cats[i].src;
-		img.alt = cats[i].alt;
+		// create cat list 
+		elem = document.createElement('li');
+		elem.innerText = cat.name;
+		catList.appendChild(elem);
 
 
-		img.addEventListener('click',(function(catCopy,counterSelectorCopy){
-			return function(){	
-			catCopy.counter++;
-			counterSelectorCopy.innerText = catCopy.counter;
+		// add event listener to each list item to display the cat 
+		elem.addEventListener("click",(function(catCl){
+			return function(){
+				name.innerText = catCl.name;
+				img.src = catCl.src;
+				img.alt = catCl.alt;
+				counterSelector.innerText = catCl.counter;
+				currentCat = catCl;
 			}
-		})(cats[i],counterSelector),false);
+		})(cat),false);
+	
+		}
 
-	}
+	// add event listener to the image for counting the clicks of the current image 
+	img.addEventListener('click',function(){
+			currentCat.counter++;
+			counterSelector.innerText = currentCat.counter;	
+		},false);
 
+	name.innerText = cats[0].name;
+	img.src = cats[0].src;
+	img.alt = cats[0].alt;
+	counterSelector.innerText = cats[0].counter;
 })();
